3ad03fc1ed44dbb598948e8f5de97ea979b02bd6
Евгений Соколов < >
2017-01-27 01:03:40 +0300

    added input

--- src/main.java
+++ src/main.java
@@ -1,8 +1,19 @@
-/**
- * Created by Shadow on 27.01.2017.
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class main {
-    public static void main(String[] args) {
-        System.out.println("init commit");
+    public static void main(String[] args) throws java.io.IOException{
+        //Вводим первый ip-адрес
+        System.out.println("Введите первый ip-адрес");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstIp = reader.readLine();
+
+        //Вводим второй ip-адрес
+        System.out.println("Введите второй ip-адрес");
+        String secondIp = reader.readLine();
+
+        //Выводим адреса
+        System.out.println(firstIp);
+        System.out.println(secondIp);
     }
 }
