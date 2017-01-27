7ec039e657b67eafa486139adae0357a1f3acafa
Евгений Соколов < >
2017-01-27 19:54:17 +0300

    checking 255 and 0 adresses

--- src/main.java
+++ src/main.java
@@ -4,20 +4,23 @@ import java.io.InputStreamReader;
 
 public class main {
     public static void main(String[] args) throws java.io.IOException{
+
         //Вводим адреса
         String firstIp = inputIp("Введите первый ip-адрес");
         String secondIp = inputIp("Введите второй ip-адрес");
+
         //Конвертируем адреса в long
-        long firstIpLong = ipToLong(firstIp);
-        long secondIpLong = ipToLong(secondIp);
+        Long firstIpLong = ipToLong(firstIp);
+        Long secondIpLong = ipToLong(secondIp);
+
         //создаем массив промежутка адрессов в long
-        long longAddresses[] = makeIpArray(firstIpLong, secondIpLong);
+        Long longAddresses[] = makeIpArray(firstIpLong, secondIpLong);
 
 
-        for (int i = 1; i < longAddresses.length; i++) {
-            System.out.println(longToIp(longAddresses[i]));
-        }
+        //выводим массив адресов
+       printArray(longAddresses);
     }
+
 
     //Конвертируем ip-адреса в Long
     public static long ipToLong(String ipAddresses)
@@ -33,6 +36,7 @@ public class main {
         }
         return result;
     }
+
     //ввод адресов
     public static String inputIp(String message) throws java.io.IOException
     {
@@ -42,36 +46,63 @@ public class main {
         return ip;
     }
 
-    public static long[] makeIpArray (long firstIp, long secondIp)
+    //Создаем массив адресов типа long
+    public static Long[] makeIpArray (Long firstIpLong, Long secondIpLong)
     {
         //Вычисляем диапозон между адресами
+        long firstIp = (long)firstIpLong;
+        long secondIp = (long)secondIpLong;
         int d = (int)secondIp - (int)firstIp;
-        long[] longAddresses = new long[d];
+        Long[] longAddresses = new Long[d+1];
+        boolean b = false;
+        int adressesLength = longAddresses.length;
 
-        for (int i = 0; i < longAddresses.length ; i++)
+        for (int i = 0; i < adressesLength ; i++)
         {
-            longAddresses[i] = firstIp + i;
+
+             if ((firstIp + i)%256==255) {
+                longAddresses[i] = firstIp + i + 2;
+                b=true;
+                adressesLength-=2;
+            }
+             if (b) {
+                longAddresses[i] = firstIp + i + 2;
+            }
+            else {
+            longAddresses[i] = firstIp + i;}
+
+            if(i == adressesLength-1)
+            {
+                longAddresses[i] = null;
+            }
         }
         return longAddresses;
     }
 
-
+    //конвертируем адреса в строку
     public static String longToIp(long ip) {
         StringBuilder result = new StringBuilder(15);
-
         for (int i = 0; i < 4; i++) {
-
             result.insert(0,Long.toString(ip & 0xff));
-
             if (i < 3) {
                 result.insert(0,'.');
             }
-
             ip = ip >> 8;
         }
         return result.toString();
     }
 
+    //вывод на экран элементов массива
+    public static void printArray(Long longAddresses[])
+    {
+        for (int i = 1; i < longAddresses.length; i++) {
+            if(longAddresses[i]==null)
+            {
+                break;
+            }
+            System.out.println(longToIp(longAddresses[i]));
+        }
+    }
 
 
 }
